c1c84f3bd878ec5e47b698c91f0c5be0bbd44319
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-10-26 15:10:38 +0200

    navigation on all buttons / pages

--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { PickupCallPage } from './pickup-call.page';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 describe('PickupCallPage', () => {
   let component: PickupCallPage;
   let fixture: ComponentFixture<PickupCallPage>;
+  let router: Router;
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule(
+      {
+        declarations: [PickupCallPage],
+        imports: [
+          IonicModule.forRoot(),
+          AppRoutingModule
+        ]
+      }).compileComponents();
 
-  beforeEach(async(() => {
     fixture = TestBed.createComponent(PickupCallPage);
+    router = TestBed.get(Router);
+
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to home page on new pickup call', () => {
+    spyOn(router, 'navigate');
+
+    component.newPickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
   });
 });
